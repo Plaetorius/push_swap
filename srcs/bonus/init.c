cc30fbdece37ee9844c6aa288fdd5e81cf4875b5
Tom GERNEZ <tgernez@student.42.fr>
2023-02-22 12:33:28 +0100

    Checker done

--- srcs/bonus/init.c
+++ srcs/bonus/init.c
@@ -6,7 +6,7 @@
 /*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 11:25:10 by tgernez           #+#    #+#             */
-/*   Updated: 2023/02/22 12:01:05 by tgernez          ###   ########.fr       */
+/*   Updated: 2023/02/22 12:13:23 by tgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,75 @@ int	init(char **av, int ac, t_push_swap *vars)
 	vars->stack_a->head = node;
 	free(table);
 	return (0);
+}
+
+void	ft_node_endings(t_node *elem, t_node *next, t_node *prev)
+{
+	if (!elem)
+		return ;
+	if (next)
+	{
+		elem->next = next;
+		next->prev = elem;
+	}
+	if (prev)
+	{
+		elem->prev = prev;
+		prev->next = elem;
+	}
+}
+
+
+t_node	*ft_node_new(int val, t_node *next, t_node *prev, int ind)
+{
+	t_node	*elem;
+
+	elem = malloc(sizeof(t_node));
+	if (!elem)
+		return (NULL);
+	elem->val = val;
+	elem->next = next;
+	elem->prev = prev;
+	elem->ind = ind;
+	return (elem);
+}
+
+int	new_instruct(int val, t_push_swap *vars)
+{
+	t_instruct	*instruct;
+	t_instruct	*tmp;
+
+	instruct = malloc(sizeof(t_instruct));
+	if (!instruct)
+		return (write(2, "Error\n", 6), 1);
+	instruct->val = val;
+	instruct->next = NULL;
+	tmp = vars->insts;
+	if (vars->insts == NULL)
+		vars->insts = instruct;
+	else
+	{
+		while (vars->insts->next)
+			vars->insts = vars->insts->next;
+		vars->insts->next = instruct;
+		vars->insts = tmp;
+	}
+	return (0);
+}
+
+int	is_stack_sorted(t_stack *stack)
+{
+	t_node	*node;
+	size_t	i;
+
+	node = stack->head;
+	i = 0;
+	while (i < stack->len - 1)
+	{
+		if (node->val > node->next->val)
+			return (0);
+		node = node->next;
+		i++;
+	}
+	return (1);
 }
--- srcs/bonus/main.c
+++ srcs/bonus/main.c
@@ -12,37 +12,36 @@
 
 #include "push_swap_checker.h"
 
-int	instruct_atoi(char *instruct)
+int	instruct_atoi(char *instruct, int val)
 {
-	if (!ft_strncmp(instruct, "ra", 5))
-		return (free(instruct), ROTATE_A);
-	else if (!ft_strncmp(instruct, "rb", 5))
-		return (free(instruct), ROTATE_B);
-	else if (!ft_strncmp(instruct, "sa", 5))
-		return (free(instruct), SWAP_A);
-	else if (!ft_strncmp(instruct, "sb", 5))
-		return (free(instruct), SWAP_B);
-	else if (!ft_strncmp(instruct, "rra", 5))
-		return (free(instruct), REV_ROTATE_A);
-	else if (!ft_strncmp(instruct, "rrb", 5))
-		return (free(instruct), REV_ROTATE_B);
-	else if (!ft_strncmp(instruct, "pa", 5))
-		return (free(instruct), PUSH_A);
-	else if (!ft_strncmp(instruct, "pb", 5))
-		return (free(instruct), PUSH_B);
-	else if (!ft_strncmp(instruct, "rr", 5))
-		return (free(instruct), RR);
-	else if (!ft_strncmp(instruct, "rrr", 5))
-		return (free(instruct), RRR);
-	else if (!ft_strncmp(instruct, "ss", 5))
-		return (free(instruct), SS);
-	return (free(instruct), -1);
+	if (!ft_strncmp(instruct, "ra\n", 5))
+		val = ROTATE_A;
+	else if (!ft_strncmp(instruct, "rb\n", 5))
+		val = ROTATE_B;
+	else if (!ft_strncmp(instruct, "sa\n", 5))
+		val = SWAP_A;
+	else if (!ft_strncmp(instruct, "sb\n", 5))
+		val = SWAP_B;
+	else if (!ft_strncmp(instruct, "rra\n", 5))
+		val = REV_ROTATE_A;
+	else if (!ft_strncmp(instruct, "rrb\n", 5))
+		val = REV_ROTATE_B;
+	else if (!ft_strncmp(instruct, "pa\n", 5))
+		val = PUSH_A;
+	else if (!ft_strncmp(instruct, "pb\n", 5))
+		val = PUSH_B;
+	else if (!ft_strncmp(instruct, "rr\n", 5))
+		val = RR;
+	else if (!ft_strncmp(instruct, "rrr\n", 5))
+		val = RRR;
+	else if (!ft_strncmp(instruct, "ss\n", 5))
+		val = SS;
+	return (free(instruct), val);
 }
 
 int	get_instructs(t_push_swap *vars)
 {
 	char		*instruct;
-	t_instruct	*tmp;
 	int			val_instruct;
 
 	while (1)
@@ -50,9 +49,12 @@ int	get_instructs(t_push_swap *vars)
 		instruct = get_next_line(0);
 		if (!instruct)
 			break ;
-		val_instruct = instruct_atoi(instruct);
+		val_instruct = instruct_atoi(instruct, -1);
 		if (val_instruct == -1 || new_instruct(val_instruct, vars))
+		{
+			ft_printf("ca fait de la meerde %d val insutrcts\n", val_instruct);
 			return (1);
+		}
 	}
 	return (0);
 }
@@ -98,8 +100,7 @@ int	main(int ac, char **av)
 		return (memory_cleaning(&vars), write(2, "Error\n", 6), EXIT_FAILURE);
 	if (execute_instructs(&vars) == 1)
 		return (memory_cleaning(&vars), write(2, "Error\n", 6), EXIT_FAILURE);
-	if (is_stack_sorted(vars.stack_a) && vars.stack_b == NULL
-		&& vars.stack_b->len == 0)
+	if (is_stack_sorted(vars.stack_a) && vars.stack_b->len == 0)
 		return (memory_cleaning(&vars), write(1, "OK\n", 3), EXIT_SUCCESS);
 	return (memory_cleaning(&vars), write(1, "KO\n", 3), EXIT_SUCCESS);
 }
